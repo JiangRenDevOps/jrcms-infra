847a75e3637bb95a8dfe28982367e174d3d69301
Davis <davis.l.liu@gmail.com>
2020-02-11 20:44:30 +1100

    test

--- terraform/4.app/scripts/init_script.sh
+++ terraform/4.app/scripts/init_script.sh
@@ -7,6 +7,7 @@ apt install -y ansible
 
 ansible-galaxy install geerlingguy.docker
 ansible-galaxy install geerlingguy.pip
+ansible-galaxy install geerlingguy.filebeat
 
 git clone https://github.com/davisliu11/jrcms-infra.git /jrcms-infra
 
